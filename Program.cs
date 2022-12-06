88f514290b95a9ee9afb83144f3da54b1308d12a
kerokero014 <101151724+kerokero014@users.noreply.github.com>
2022-12-05 17:16:10 -0700

    changes

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using jumper.Game;
+
+namespace jumper
+{
+
+    public class Program 
+    {
+
+        public static void Main(string[] args)
+        {
+            
+            Director dir = new Director();
+            dir.StartGame();
+
+        }
+
+    }
+
+}
